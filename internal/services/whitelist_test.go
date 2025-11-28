73674e4497e18e0f1b89961e55cae7ff666378eb
tekikaito <marc-gilbrecht@outlook.de>
2025-11-28 18:01:49 +0100

    feat: add AddNameToWhitelist function and corresponding tests in WhitelistService

--- internal/services/whitelist_test.go
+++ internal/services/whitelist_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -118,4 +119,109 @@ func TestWhitelistService_RemoveNameFromWhitelist(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestWhitelistService_AddNameToWhitelist(t *testing.T) {
+	tests := []struct {
+		name              string
+		inputName         string
+		existingWhitelist []string
+		mojangExists      bool
+		mojangErr         error
+		expectedCmd       string
+		executeOutput     string
+		executeErr        error
+		wantErr           bool
+	}{
+		{
+			name:              "valid name",
+			inputName:         "Steve",
+			existingWhitelist: []string{},
+			mojangExists:      true,
+			expectedCmd:       "whitelist add Steve",
+		},
+		{
+			name:      "empty name",
+			inputName: "   ",
+			wantErr:   true,
+		},
+		{
+			name:              "name already whitelisted",
+			inputName:         "Alex",
+			existingWhitelist: []string{"Alex"},
+			wantErr:           true,
+		},
+		{
+			name:      "mojang check error",
+			inputName: "Herobrine",
+			mojangErr: errors.New("boom"),
+			wantErr:   true,
+		},
+		{
+			name:         "mojang name does not exist",
+			inputName:    "NotARealPlayer",
+			mojangExists: false,
+			wantErr:      true,
+		},
+		{
+			name:         "rcon error",
+			inputName:    "Zombie",
+			mojangExists: true,
+			expectedCmd:  "whitelist add Zombie",
+			executeErr:   errors.New("boom"),
+			wantErr:      true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fakeRconClient := &fakeRconClient{
+				responses: map[string]struct {
+					out string
+					err error
+				}{
+					"whitelist list": {out: formatWhitelistListOutput(tt.existingWhitelist), err: nil},
+					tt.expectedCmd:   {out: tt.executeOutput, err: tt.executeErr},
+				},
+			}
+			fakeMojangChecker := &fakeMojangChecker{
+				existsMap: map[string]bool{},
+				errMap:    map[string]error{},
+			}
+			fakeMojangChecker.existsMap[tt.inputName] = tt.mojangExists
+			if tt.mojangErr != nil {
+				fakeMojangChecker.errMap[tt.inputName] = tt.mojangErr
+			}
+
+			svc := NewWhitelistService(fakeRconClient, fakeMojangChecker)
+			err := svc.AddNameToWhitelist(tt.inputName)
+			if tt.wantErr {
+				if err == nil {
+					t.Fatalf("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+		})
+	}
+}
+
+func formatWhitelistListOutput(names []string) string {
+	if len(names) == 0 {
+		return "There are 0 whitelisted player(s):"
+	}
+	return "There are " + fmt.Sprintf("%d", len(names)) + " whitelisted player(s): " + joinNames(names)
+}
+
+func joinNames(names []string) string {
+	result := ""
+	for i, name := range names {
+		if i > 0 {
+			result += ", "
+		}
+		result += name
+	}
+	return result
 }
